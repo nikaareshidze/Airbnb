51520df73806381aa8e294c121840b764907f593
nikaareshidze <nikaareshidzeapps@gmail.com>
2022-12-23 19:20:06 +0400

    test commit

--- components/Filter/FilterDataItem.jsx
+++ components/Filter/FilterDataItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FilterDataItem({ item }) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center justify-start">
       <img src={`${item.image}`} className="w-6 h-6 mb-2" />
       <h1 className="text-xs text-center whitespace-nowrap">{item.title}</h1>
     </div>
